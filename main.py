2a44d06c2aaa682b79c6b983bd0b0c83dd9677a8
Roxxi906 <115946419+Roxxi906@users.noreply.github.com>
2022-12-12 15:30:14 +0000

    auto update

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import pygame
+import random
 
 # Připravíme PyGame
 pygame.init()
-pygame.display.set_caption(".....")
-size = (1920, 720)
+pygame.display.set_caption("  Get fuel")
+size = (490, 320)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 # Připravíme si obrázek
-background = (69, 255, 69)
-auto = pygame.image.load("brr2.png")
-auto = pygame.transform.scale(auto, (40, 40))
+background = (200, 200, 200 )
+turtle = pygame.image.load("turtle.png")
+turtle = pygame.transform.scale(turtle, (40, 40))
+pineapple = pygame.image.load("pineapple.png")
+pineapple = pygame.transform.scale(pineapple, (40, 40))
+Trava = pygame.image.load("Tráva.png")
+Trava = pygame.transform.scale(Trava,(400, 265))
+dlaždice = pygame.Surface((40, 40))
+dlaždice.set_alpha(128)
+dlaždice.fill((255, 255, 255))
 
-WIDTH = 17
-HEIGHT = 14
+# screen.blit(turtle, game_to_screen(zx, zy))
+# screen.blit(pineapple, game_to_screen(0, 0))                                                                   
 
-zx = 1
+# Připravíme si obrázek
+
+WIDTH = 9
+HEIGHT = 6
+zx = 2
 zy = 1
-
 
 def game_to_screen(x, y):
     return (x * 45, y * 45)
@@ -37,7 +48,7 @@ def animate(nx, ny):
         x = old * sx + new * ex
         y = old * sy + new * ey
         screen.fill(background)
-        draw_grid()
+        draw_scene()
         screen.blit(rotated_turtle, (x, y))
         pygame.display.update()
         t = t + clock.tick()
@@ -45,36 +56,45 @@ def animate(nx, ny):
     zx = nx
     zy = ny
 
-def draw_grid():
-    for x in range(WIDTH):
+def draw_scene():
+  screen.fill(background)
+  screen.blit(Trava,(0, 0))
+  for x in range(WIDTH):
         for y in range(HEIGHT):                    
-            color = (255, 255, 255)
-            rect = (game_to_screen(x, y), (40, 40))
-            pygame.draw.rect(screen, color, rect)
+           screen.blit(dlaždice,game_to_screen(x, y))          
+  screen.blit(pineapple, game_to_screen(pa_x, pa_y))
 
-rotated_auto = pygame.transform.rotate(auto, 0)
+pa_x = 0
+pa_y = 0
+
+rotated_turtle = pygame.transform.rotate(turtle, 0)
 while True:
     # Vykreslíme
-    screen.fill(background)
-    draw_grid()
-    screen.blit(rotated_auto, game_to_screen(zx, zy))
+    
+    draw_scene()    
+    screen.blit(rotated_turtle, game_to_screen(zx, zy))
+    
     pygame.display.update()
 
     event = pygame.event.wait()
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_RIGHT:
-            rotated_turtle = pygame.transform.rotate(auto, 270)
+            rotated_turtle = pygame.transform.rotate(turtle, 180)
             if zx < WIDTH - 1:
                 animate(zx + 1, zy)
         elif event.key == pygame.K_UP:
-            rotated_turtle = pygame.transform.rotate(auto, 0)
+            rotated_turtle = pygame.transform.rotate(turtle, 270)
             if zy > 0:
                 animate(zx, zy - 1)
         elif event.key == pygame.K_LEFT:
-            rotated_turtle = pygame.transform.rotate(auto, 90)
+            rotated_turtle = pygame.transform.rotate(turtle, 0)
             if zx > 0:
                 animate(zx - 1, zy)
         elif event.key == pygame.K_DOWN:
-            rotated_turtle = pygame.transform.rotate(auto, 180)
+            rotated_turtle = pygame.transform.rotate(turtle, 90)
             if zy < HEIGHT - 1:
                 animate(zx, zy + 1)
+
+    if (zx, zy) == (pa_x, pa_y):
+        pa_x = random.randrange(0, WIDTH)
+        pa_y = random.randrange(0, HEIGHT)
